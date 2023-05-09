4beeb6e0dc06b143a73a94c1047d70ed9c910d34
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-05-10 00:06:40 +0530

    Add click inputs on buttons

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+let buttonsList = Array.from(document.querySelectorAll('button'));
+buttonsList.splice(0,1);
 
+let allClearButton = document.querySelector('.ac-operation');
+
+
+function inputVal(element) {
+    document.querySelector('.display-text').textContent += element.textContent; 
+}
+
+
+
+buttonsList.forEach(element => {element.addEventListener("click", () => {
+    document.querySelector('.display-text').textContent += element.textContent; 
+    }
+)});
     
+allClearButton.addEventListener('click', () => {
+    document.querySelector('.display-text').textContent = 0;
+})
