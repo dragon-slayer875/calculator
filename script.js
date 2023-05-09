76f8a774dbe3fc08503033e0595b67b700a5ae6f
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-05-10 00:52:05 +0530

    Implement the all clear button's function

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ buttonsList.splice(0,1);
 
 let allClearButton = document.querySelector('.ac-operation');
 
-
-function inputVal(element) {
-    document.querySelector('.display-text').textContent += element.textContent; 
-}
-
-
-
 buttonsList.forEach(element => {element.addEventListener("click", () => {
+    if (document.querySelector('.display-text').textContent == 0) {
+        document.querySelector('.display-text').textContent = '';
+    }
     document.querySelector('.display-text').textContent += element.textContent; 
     }
 )});
