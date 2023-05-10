2e0e395b7e7c8cdedc947fc592aa265f36a13fed
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-05-10 23:18:18 +0530

    Add delete function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let buttonsList = Array.from(document.querySelectorAll('button'));
-buttonsList.splice(0,1);
+buttonsList.splice(0,2);
 
 let allClearButton = document.querySelector('.ac-operation');
+let deleteButton = document.querySelector('.delete-operation');
 
 buttonsList.forEach(element => {element.addEventListener("click", () => {
     if (document.querySelector('.display-text').textContent == 0) {
@@ -10,6 +11,13 @@ buttonsList.forEach(element => {element.addEventListener("click", () => {
     document.querySelector('.display-text').textContent += element.textContent; 
     }
 )});
+
+deleteButton.addEventListener('click', () => {
+    if (document.querySelector('.display-text').textContent == 0) {
+        return;
+    }
+    document.querySelector('.display-text').textContent = document.querySelector('.display-text').textContent.slice(0, -1);
+})
     
 allClearButton.addEventListener('click', () => {
     document.querySelector('.display-text').textContent = 0;
