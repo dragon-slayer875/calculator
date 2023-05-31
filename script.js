60bbc856330ddca4b08a82b507c110be531d30c7
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-06-01 00:28:42 +0530

    Add delete functionality through keyboard

--- script.js
+++ script.js
@@ -58,3 +58,45 @@ evalButton.addEventListener('click', () => {
     }
     document.querySelector('.display-text').textContent = result;
 })
+
+document.querySelector('body').addEventListener('keypress', (e) => {
+    if (!(/[a-zA-Z]/.test(e.key))){
+        if (document.querySelector('.display-text').textContent == 0) {
+            document.querySelector('.display-text').textContent = '';
+        }    
+        if (/[-*+\/%]/.test(e.key)) {
+            /[-*+\/%]/.test(document.querySelector('.display-text').textContent.slice(-1))?
+            document.querySelector('.display-text').textContent += '':
+            document.querySelector('.display-text').textContent += ' ' + e.key;
+        } 
+        else {
+            /[-*+\/%]/.test(document.querySelector('.display-text').textContent.slice(-1))? 
+            document.querySelector('.display-text').textContent += ' ' + e.key: 
+            document.querySelector('.display-text').textContent += e.key;
+        }}
+    if (e.key == 'Enter') { 
+        let operationStack = document.querySelector('.display-text').textContent.split(' ');
+        for (let index = 0; index < operationStack.length; index++) {
+            if (operationStack[index] in operations) {
+                result = operations[operationStack[index]](new Number(operationStack[index-1]), new Number(operationStack[index+1]));
+                operationStack.splice(0, 3, result);
+                index-=1;
+            }
+        }
+        document.querySelector('.display-text').textContent = result;
+    }   
+})
+
+document.querySelector('body').addEventListener('keydown', (e) => {
+    if (e.key == 'Backspace' || e.key == 'Delete') {
+        if (document.querySelector('.display-text').textContent.length == 1) {
+            document.querySelector('.display-text').textContent = 0;
+        }
+        if (document.querySelector('.display-text').textContent == 0) {
+            return;
+        }
+        /\s/.test(document.querySelector('.display-text').textContent.slice(-2))?
+        document.querySelector('.display-text').textContent = document.querySelector('.display-text').textContent.slice(0, -2):
+        document.querySelector('.display-text').textContent = document.querySelector('.display-text').textContent.slice(0, -1);
+    }  
+})
